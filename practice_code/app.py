ec9900a834395e039cdfe3729f1d9f36c91ed2ee
Marc Blackmer <marc.blackmer@1nterrupt.com>
2021-04-18 21:12:11 -0400

    Updates

--- practice_code/app.py
+++ practice_code/app.py
@@ -1,5 +1,12 @@
-from flask import Flask, request, abort
+from flask import Flask, request, abort, jsonify
+import json
 from functools import wraps
+from jose import jwt
+from urllib.request import urlopen
+
+AUTH0_DOMAIN = 'blackmer.us.auth0.com'
+ALGORITHMS = ['RS256']
+API_AUDIENCE = 'fsnd'
 
 
 def get_token_auth_header():
@@ -24,23 +31,50 @@ def get_token_auth_header():
     return header_parts[1]
 
 
-def requires_auth(f):
-    @wraps(f)
-    def wrapper(*args, **kwargs):
-        jwt = get_token_auth_header()
-        return f(jwt, *args, **kwargs)
-    return wrapper
+def check_permissions(permission, payload):
+    if 'permissions' not in payload:
+        raise AuthError({
+            'code': 'invalid_claims',
+            'description': 'Permissions not included in JWT.'
+        }, 400)
+
+    if permission not in payload['permissions']:
+        raise AuthError({
+            'code': 'unauthorized',
+            'description': 'Permission not found.'
+        }, 403)
+    return True
+
+
+def requires_auth(permission=''):
+    def requires_auth_decorator(f):
+        @wraps(f)
+        def wrapper(*args, **kwargs):
+            jwt = get_token_auth_header()
+            try:
+                payload = verify_decode_jwt(jwt)
+            except:
+                abort(401)
+
+            check_permissions(permission, payload)
+
+            return f(payload, *args, **kwargs)
+        return wrapper
+    return requires_auth_decorator
 
 
 app = Flask(__name__)
 
 
-@app.route('/headers')
+@app.route('/login-results')
 @requires_auth
 def headers(jwt):
     print(jwt)
     return 'not implemented'
 
-@app.route('/list')
-def get_list():
+
+@app.route('/images')
+@requires_auth
+def images(jwt):
+    print(jwt)
     return 'not implemented'
